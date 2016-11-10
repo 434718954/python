9439e27641c13895b3c163a5237f458cb7de261b
Charles Lee <393130067@qq.com>
2016-11-10 15:32:07 +0800

    Update test_classifly.py
    训练预测

--- text_classify/test_classifly.py
+++ text_classify/test_classifly.py
@@ -4,7 +4,7 @@ import featrue_pj as fj
 from os.path import dirname 
 from os.path import join
 from sklearn.cross_validation import   train_test_split  #k折交叉模块  
-from sklearn.naive_bayes import MultinomialNB
+from sklearn.naive_bayes import BernoulliNB
 from sklearn import metrics
 if __name__ == '__main__':
 	
@@ -31,11 +31,11 @@ if __name__ == '__main__':
 	fs=fj.Feature_select()
 	term_vec=fs.transform(term_str)
 	x_train,x_test,y_train,y_test=train_test_split(term_vec,class_list,test_size=0.2) 
-	clf = MultinomialNB().fit(x_train, y_train)  #调用MultinomialNB分类器
-#	pexpected = y_test
-#	ppredicted = clf.predict(x_test)
-#	print(metrics.classification_report(pexpected, ppredicted))
-#	print(metrics.confusion_matrix(pexpected, ppredicted))
-	term_vec=fs.transform(['咖啡'])
-	ppredicted = clf.predict(term_vec)
-	print ppredicted[0]
+	clf = BernoulliNB().fit(x_train, y_train)  #调用MultinomialNB分类器
+	pexpected = y_test
+	ppredicted = clf.predict(x_test)
+	print(metrics.classification_report(pexpected, ppredicted))
+	print(metrics.confusion_matrix(pexpected, ppredicted))
+#	term_vec=fs.transform(['咖啡'])
+#	ppredicted = clf.predict(term_vec)
+#	print ppredicted[0]
